9964f096fc3cd8e22def4fa296e9869225a6350b
Agent0876 <shinseungmin070920@gmail.com>
2026-01-12 22:44:40 +0900

    first commit

---
+++ src/main/kotlin/io/github/jiwontechinovation/jio/JiaaAuthApplication.kt
@@ -0,0 +1,11 @@
+package io.github.jiwontechinovation.jio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JiaaAuthApplication
+
+fun main(args: Array<String>) {
+    runApplication<JiaaAuthApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/jiwontechinovation/jio/JiaaAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.jiwontechinovation.jio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JiaaAuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
